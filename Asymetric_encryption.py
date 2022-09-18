18943c9e7d33e209ad08c39d7f4d841cfba0f5a3
adityaparihar7 <adityaparihar777@gmail.com>
2022-09-18 11:33:29 +0530

    Hashing password file added
    New files

--- Asymetric_encryption.py
+++ Asymetric_encryption.py
@@ -1,3 +1,7 @@
+# Asymmetric is a cryptographic algorithm that requires two separate keys, one private and the other public. 
+# The message is encrypted with the public key and decrypted with the private key. 
+# As a result, anyone with the public key can encrypt but not decrypt information. 
+# The information can only be decrypted by someone who has the corresponding private key.
 import rsa
 from rsa.key import PrivateKey, PublicKey
 
@@ -11,3 +15,4 @@ print(encryption_message)
 
 decrypt_message = rsa.decrypt(encryption_message, privateKey).decode()
 print(encryption_message)
+
